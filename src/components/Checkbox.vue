78404809463d6a99b889c88a299fc7fef2f54d6d
kasyap-007 <70818272+kasyap-007@users.noreply.github.com>
2023-01-09 00:22:30 +0530

    2 files modified

--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -1,16 +1,23 @@
 <template>
   <h1>Vue</h1>
-  <input type="checkbox" id="checkbox" v-model="checked" />
-  <label for="checkbox">{{ checked }}</label>
-  <button @click="count++">Count is: {{ count }}</button>
+  <label for="checkbox">
+    Checkbox
+    <input type="checkbox" id="checkbox" v-model="checked" />
+  </label>
+  {{ checked ? 'hi' : '' }}
+  <input type="date" v-if="checked" />
+
+  <!-- <button @click="count++">Count is: {{ count }}</button> -->
 </template>
 
 <script>
 export default {
   name: 'Checkbox',
+
   data() {
     return {
-      count: 0,
+      // count: 0,
+      checked: true,
     };
   },
 };
